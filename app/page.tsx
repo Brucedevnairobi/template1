d98f693c43d72f2428d2d0b93f8df98895096bf8
BRUCE SIMIYU <bruce14simiyu@gmail.com>
2024-01-29 16:47:39 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="flex h-5  items-center bg-amber-600 p-8 font-bold text-red-600">
+        Hello World
+      </h1>
+    </div>
+  );
+}
